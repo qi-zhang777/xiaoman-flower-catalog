724ff18db16ca9aa6e2324312a1eb9faafa78212
镇淇张 <1791996977@qq.com>
2026-08-20 21:50:56 +0800

    Fix Windows catalog editor launcher

--- 编辑图册.cmd
+++ 编辑图册.cmd
@@ -1,11 +1,2 @@
-@echo off
-chcp 65001 >nul
-cd /d "%~dp0"
-echo 正在打开小满 flower 图册编辑器……
-py -3 catalog_manager.py
-if errorlevel 1 (
-  echo.
-  echo 启动失败，请确认电脑已经安装 Python。
-  pause
-)
+@echo off & cd /d "%~dp0" & py -3 catalog_manager.py
 
